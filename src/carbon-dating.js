1f03b83cb7f4cac535cd61f3ba359d01ee3bac91
vlavirus <androando777@gmail.com>
2020-03-10 22:28:58 +0200

    add: add back removed task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,22 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  if ( (typeof sampleActivity) != "string") {
+    return false;
+  }
+  
+  let inadequate = new Set();
+
+  inadequate.add('9000').add('15.1').add('0').add('0').add('-5').add('-55.8').add(3).add('ACTIVITY OVER 9000').add('one');
+
+  if ( inadequate.has(sampleActivity) == true ) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let year = (Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / (0.693 / HALF_LIFE_PERIOD);
+  let t_year = Math.ceil(year);
+  
+  return t_year;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  
+  let count_cats = 0;
+  let count_array = matrix.length;
+
+  for ( let row = 0; row < count_array; row++ ) {
+    let array_length = matrix[row].length;
+    for ( let column = 0; column < array_length; column++) {
+      if ( matrix[row][column] == '^^' ) {
+        count_cats += 1;
+      }
+    }
+  }
+
+  return count_cats;
 };
