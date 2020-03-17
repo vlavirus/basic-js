00b229e69785c7eea61de3da25041a0798c0531e
vlavirus <androando777@gmail.com>
2020-03-17 23:21:58 +0200

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,38 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options) {
+    let repeatTimes = options.repeatTimes;
+    let separator;
+    let addition;
+    let newAddition;
+    let additionSeparator;
+    let additionRepeatTimes = options.additionRepeatTimes;
+
+    if ( !(options.separator)) {
+        separator = '+';
+    } else {
+        separator = (''+options.separator);
+    }
+    if ((options.addition) === undefined && typeof options.addition !== 'string') {
+        newAddition = '';
+        addition = '';
+    } else {
+        addition = (''+options.addition);
+        newAddition = (''+options.addition);
+    }
+    if (!(options.additionSeparator)) {
+        additionSeparator = '';
+    } else {
+        additionSeparator = (''+options.additionSeparator);
+    }
+    
+    for ( let i = 0; i < additionRepeatTimes - 1; i++ ) {
+        newAddition += additionSeparator + addition;
+    }
+
+    let answer = (''+str)+newAddition;
+
+    for ( let i = 0; i < repeatTimes - 1; i++) {
+        answer += separator+str+newAddition;;
+    }
+    return answer;
 };
   
