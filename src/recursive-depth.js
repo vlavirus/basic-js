1a4a2271636d9699fc20415358a5f42eb3c9e2d7
vlavirus <androando777@gmail.com>
2020-03-19 23:45:23 +0200

    fear: add recurisve-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,14 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth( arr ) {
+        let arrays = 0;
+        let depth = 0;
+       
+        for (let k of arr) {
+           if ( Array.isArray(k)) {
+            arrays = this.calculateDepth(k);
+           }
+           if ( depth < arrays ) depth = arrays;
+        }
+    return depth+=1;
     }
 };
