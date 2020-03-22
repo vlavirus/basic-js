fcdbbe4ac69475442a526217a2734633d9efde46
vlavirus <androando777@gmail.com>
2020-03-22 16:09:01 +0200

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push('( '+value+' )')
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if ( typeof position !== 'number' || position > this.chain.length  || position < 1 ) {
+      this.chain = [];
+      throw new Error("Wrong");
+    }
+    this.chain.splice(position - 1,1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chainBack = this.chain.join('~~');
+    this.chain = [];
+    return chainBack;
   }
 };
 
