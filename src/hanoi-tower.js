7d5f2685a47797ab3822c3c1370601eeb64896c7
vlavirus <androando777@gmail.com>
2020-03-22 18:20:23 +0200

    feat: add calculateHanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,10 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi (disksNumber, turnsSpeed ) {
+    let answer = {}
+    let turns = Math.pow(2, disksNumber);
+    let seconds = (turnsSpeed / 3600);
+    
+    answer.turns = turns;
+    answer.seconds = turns / seconds;
+    
+    return answer;   // remove line with error and write your code here
 }
